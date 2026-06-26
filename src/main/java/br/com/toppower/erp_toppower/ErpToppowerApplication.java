ae165180c766060fd0b8dc78b00f0c00613ddcaf
CaioAntonioJava <caioantonio.dev@gmail.com>
2026-06-26 12:39:21 -0300

    chore: initialize project

---
+++ src/main/java/br/com/toppower/erp_toppower/ErpToppowerApplication.java
@@ -0,0 +1,12 @@
+package br.com.toppower.erp_toppower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpToppowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpToppowerApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/toppower/erp_toppower/ErpToppowerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.toppower.erp_toppower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpToppowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
