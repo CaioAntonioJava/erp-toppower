ea078513b3ace2352d98f5c9736a4d48085da433
CaioAntonioJava <caioantonio.dev@gmail.com>
2026-07-01 13:06:26 -0300

    chore(client): remove legacy Client module replaced by Company and Customer
    The Client module mixed PJ and PF in a single entity behind a
    PersonType flag. That responsibility has been split:
    - Company (PJ) -> br.com.toppower.erp_toppower.company
    - Customer (PF) -> br.com.toppower.erp_toppower.customer
    
    Removes the entire br.com.toppower.erp_toppower.client package
    (controller, service, repository, entity, mapper, DTOs, enums and
    exceptions). The legacy 'clients' table is dropped by the V1
    migration added in a previous commit.

--- backend/src/main/java/br/com/toppower/erp_toppower/client/exception/DuplicateClientCodeException.java
+++
@@ -1,8 +0,0 @@
-package br.com.toppower.erp_toppower.client.exception;
-
-public class DuplicateClientCodeException extends RuntimeException {
-
-    public DuplicateClientCodeException(String code) {
-        super("Já existe um cliente cadastrado com o código: " + code);
-    }
-}
--- backend/src/main/java/br/com/toppower/erp_toppower/client/exception/DuplicateClientTaxIdException.java
+++
@@ -1,8 +0,0 @@
-package br.com.toppower.erp_toppower.client.exception;
-
-public class DuplicateClientTaxIdException extends RuntimeException {
-
-    public DuplicateClientTaxIdException(String taxId) {
-        super("Já existe um cliente cadastrado com o CPF/CNPJ: " + taxId);
-    }
-}
